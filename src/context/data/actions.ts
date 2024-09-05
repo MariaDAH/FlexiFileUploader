9b8d1c67f4557a9459f61fe84ce1a4e871850326
MDAH <deandresherrero@gmail.com>
2024-09-05 18:54:01 +0100

    Uploader fix path to public folder

--- src/context/data/actions.ts
+++ src/context/data/actions.ts
@@ -1,56 +1,17 @@
 "use server"
 import { promises as fs } from 'fs';
 import { revalidatePath } from "next/cache";
+import path from 'path';
 
 export async function uploadFiles(formData: FormData) {
     const file = formData.get("file") as File;
-    console.log('FORM DATA**********',file);
+    //console.log('FORM DATA**********',file);
     const arrayBuffer = await file.arrayBuffer();
     const buffer = new Uint8Array(arrayBuffer);
 
-    await fs.writeFile(`./public/uploads/${file.name}`, buffer);
+    const path_ = path.join(process.cwd(), './public/uploads/');
+
+    await fs.writeFile(`${path_}${file.name}`, buffer);
 
     revalidatePath("/");
 }
-
-/*export async function saveFiles(files:string[]) {
-    const file = await fs.readFile(process.cwd() + '/src/utils/mock-data/files.json', 'utf8');
-    const data = JSON.parse(file);
-    console.log(data);
-
-    files.forEach(async (file) => {
-        let blob = await getImageFromURL(file);
-    })
-}*/
-
-
-/*async function getImageFromURL(imageUrl: string) {
-
-    const response =  fetch(imageUrl, {
-        method: 'GET',
-        headers: {}
-    })
-
-    const arrayBuffer = await response.then(
-        res => res.arrayBuffer()
-    )
-
-    console.log(arrayBuffer, "arrayBuffer");
-
-    URL.revokeObjectURL(imageUrl);
-
-    //const blob = new Blob([arrayBuffer, { type: 'image/png' }]);
-
-    //const file = convertBlobToFile(blob, 'image.png')
-
-    //uploadImage(file)
-
-    // const url = URL.createObjectURL(blob);
-
-
-    function convertBlobToFile(blob, fileName){
-        blob.lastModifiedDate = new Date();
-        blob.name = fileName;
-        return blob;
-    }
-}*/
