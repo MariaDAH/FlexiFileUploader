5a40807f39a3238dd412f3b708a88b13eb031014
MDAH <deandresherrero@gmail.com>
2024-09-06 08:27:43 +0100

    Adding vercel blob storage

--- src/context/data/actions.ts
+++ src/context/data/actions.ts
@@ -1,10 +1,12 @@
 "use server"
+import { put } from '@vercel/blob';
 import { promises as fs } from 'fs';
 import { revalidatePath } from "next/cache";
 import path from 'path';
 
-export async function uploadFiles(formData: FormData) {
+export async function uploadFiles(formData: FormData): Promise<void> {
     const file = formData.get("file") as File;
+
     //console.log('FORM DATA**********',file);
     const arrayBuffer = await file.arrayBuffer();
     const buffer = new Uint8Array(arrayBuffer);
@@ -16,6 +18,19 @@ export async function uploadFiles(formData: FormData) {
     console.log("Saving file in path", fullPath);
 
     await fs.writeFile(`${path_}${file.name}`, buffer);
+
+    revalidatePath("/");
+}
+
+
+export async function uploadBlob(formData: FormData): Promise<void> {
+
+    const image = formData.get("file") as File;
+    const blob = await put(image.name, image, {
+        access: "public",
+    });
+
+    console.log(blob);
 
     revalidatePath("/");
 }
