9841bd28a73b22bd5ed5b2e34b3f4798a107af1e
MDAH <deandresherrero@gmail.com>
2024-09-08 15:25:28 +0100

    Miscelaneous
    * Fix issue with load table in the client

--- src/app/(home)/(dashboard)/home/DocumentsPanel.tsx
+++ src/app/(home)/(dashboard)/home/DocumentsPanel.tsx
@@ -1,45 +1,16 @@
 "use client"
 import FilesTable from "./FilesTable";
-import Loader from "@/components/ui/loader/loader";
-import {useEffect, useState } from "react";
 import {File} from "@/context/interfaces/file"
 
 export default function DocumentsPanel( {data}: { data: File[] } ) {
-
-    const [documents, setDocuments] = useState<File[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<Error | null>(null)
-
-    useEffect(() => {
-        if (data) {
-            setLoading(false);
-            setDocuments(data);
-        };
-
-        if (error) {
-            setError(error);
-        }
-    }, [data, error]);
 
     return (
         <>
             <section className="flex items-center">
                 {
-                    loading ? (
-                        <div className="flex justify-center items-center">
-                            <Loader/>
-                        </div>
-                    ) : error ? (
-                        <div>Error loading blobs: {error?.message}</div>
-                    ) : (
-                        documents ? (
-                            <div className="h-screen flex justify-center">
-                                <FilesTable rows={documents}></FilesTable>
-                            </div>
-                        ) : (
-                            <p>Alert: Info: No data fetch.</p>
-                        )
-                    )
+                    <div className="h-screen flex justify-center">
+                        <FilesTable rows={data.map(x => x)}></FilesTable>
+                    </div>
                 }
             </section>
         </>
